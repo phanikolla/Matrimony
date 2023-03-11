75e7e3d6079d7e640755c3264dd8337a63a8c320
phani <phani.kollas@gmail.com>
2023-03-11 23:11:34 +0530

    Inital Cut

---
+++ matrimony-app/src/components/MyComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MyComponent() {
+    return (
+        <div>
+            <h1>This is a Matrimonal App with Springboot, React & Mongo!</h1>
+        </div>
+    );
+}
+
+export default MyComponent;
